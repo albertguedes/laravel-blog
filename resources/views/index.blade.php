abf245c6af9c015d4bdd83cd9db8e4a5057d134b
Albert R. Carnier Guedes <albert@teko.net.br>
2021-06-25 16:52:57 -0300

    More changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
         @foreach( $posts as $post )
         <div class="col-12 pb-5" >
             <h2><a href="{{ route('post',['post'=>$post]) }}" >{{ $post->title }}</a></h2>
-            <h6 class="text-black-50" ><i class="fas fa-clock"></i> {{ $post->created_at->format("Y M d") }}</h6>
+            <h6 class="text-black-50" ><i class="fas fa-calendar-alt"></i> {{ $post->created_at->format("Y M d") }}</h6>
             <div class="py-3" >
                 {{ substr($post->content,0,140) }} ...
                 <p><a href="{{ route('post',['post'=>$post]) }}" >Read More</a></p>
