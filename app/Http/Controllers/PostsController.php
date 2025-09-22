b9b68f6aa2af80bd8bdf5280ef03949418211928
albert r. carnier guedes <albert@teko.net.br>
2025-09-22 00:29:01 -0300

    change: added get params for archive

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,10 +1,12 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Symfony\Component\HttpFoundation\Response;
+
+use App\Models\Post;
 
 class PostsController extends Controller
 {
@@ -22,12 +24,12 @@ class PostsController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Contracts\View\View|Response
+     * @return \Illuminate\Contracts\View\View
      */
-    public function show (Post $post): View|Response
+    public function show (Post $post): View
     {
         if (!$post->exists() || !$post->published) {
-            return response('errors.404', Response::HTTP_NOT_FOUND);
+            return view('errors.404', Response::HTTP_NOT_FOUND, );
         }
 
         return view('post',compact('post'));
@@ -38,7 +40,30 @@ class PostsController extends Controller
      *
      * @return \Illuminate\Contracts\View\View
      */
-    public function archive(): View {
-        return view('archive');
+    public function archive(Request $request): View
+    {
+        if ($request->get('month') && !$request->get('year')) {
+            return view('404');
+        }
+
+        if ($request->get('day') && !$request->get('month') || $request->get('day') && !$request->get('year')) {
+            return view('404');
+        }
+
+        $year = 0;
+        $month = 0;
+        $day = 0;
+
+        if ($request->get('year')) {
+            $year = $request->get('year');
+            if ($request->get('month')) {
+                $month = $request->get('month');
+                if ($request->get('day')) {
+                    $day = $request->get('day');
+                }
+            }
+        }
+
+        return view('archive', compact('year','month','day'));
     }
 }
