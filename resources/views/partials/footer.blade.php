59ec60dcc17974a6c5ecade134a0503d921b4585
albert r. carnier guedes <albert@teko.net.br>
2026-01-10 17:25:02 -0300

    change: add chat and search links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -13,6 +13,12 @@
             <div class="mb-3 col-3" >
                 <a class="text-decoration-none text-secondary" href="{{ route('rss') }}" ><i class="fas fa-rss"></i> Follow-us</a>
             </div>
+        </div>
+
+        <div class="row" >
+            <div class="mb-3 col-3" >
+                <a class="text-decoration-none text-secondary" href="{{ route('search') }}" ><i class="fas fa-search"></i> Search</a>
+            </div>
             <div class="mb-3 col-3" >
                 <a class="text-decoration-none text-secondary" href="{{ route('categories') }}" ><i class="fas fa-sitemap"></i> Categories</a>
             </div>
@@ -22,32 +28,46 @@
             <div class="mb-3 col-3" >
                 <a class="text-decoration-none text-secondary" href="{{ route('archive') }}" ><i class="fas fa-archive"></i> Archive</a>
             </div>
+        </div>
+
+        <div class="row" >
+            <div class="mb-3 col-3" >
+                <a class="text-decoration-none text-secondary" href="{{ route('chat') }}" ><i class="fas fa-comments"></i> Chat</a>
+            </div>
             <div class="mb-3 col-3" >
                 <a class="text-decoration-none text-secondary" href="{{ route('authors') }}" ><i class="fas fa-users"></i> Authors</a>
             </div>
         </div>
+
     </div>
-    <div class="pt-4 col-2" >
-        <div class="pt-1 row" >
+
+    <div class="p-0 col-2 align-self-center" >
+        <ul class="p-0 w-100 list-group list-group-horizontal" >
             @auth
-            <div class="pt-2 col-6" >
-                <a class="text-primary text-decoration-none" href="{{ route('profile') }}" ><i class="fas fa-user-circle"></i> Profile</a>
-            </div>
-            <div class="p-0 col-6" >
+            <li class="list-item p-0 border-0 w-100" >
+                <a class="list-link text-primary text-decoration-none" href="{{ route('profile') }}" >
+                    <i class="fas fa-user-circle"></i> Profile
+                </a>
+            </li>
+            <li class="list-item p-0 border-0 w-100" >
                 <form method="POST" action="{{ route('logout') }}" class="d-inline">
                     @csrf
                     <button class="btn btn-danger text-decoration-none" type="submit"><i class="fas fa-sign-out-alt"></i> Logout</button>
                 </form>
-            </div>
+            </li>
             @else
-            <div class="pt-2 col-6" >
-                <a class="text-primary text-decoration-none" href="{{ route('login') }}" ><i class="fas fa-sign-in-alt"></i> Login</a>
-            </div>
-            <div class="p-0 pt-2 col-6" >
-                <a class="text-success text-decoration-none" href="{{ route('register') }}" ><i class="fas fa-user-plus"></i> Register</a>
-            </div>
+            <li class="p-0 border-0 list-group-item w-100" >
+                <a class="text-primary text-decoration-none" href="{{ route('login') }}" >
+                    <i class="fas fa-sign-in-alt"></i> Login
+                </a>
+            </li>
+            <li class="p-0 border-0 list-group-item w-100" >
+                <a class="text-success text-decoration-none" href="{{ route('register') }}" >
+                    <i class="fas fa-user-plus"></i> Register
+                </a>
+            </li>
             @endauth
-        </div>
+        </ul>
     </div>
 
     <div class="py-5 col-12" >
