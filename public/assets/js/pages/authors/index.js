64e5e35e034e7401e1c85821e07e4338c0cf5934
albert r. carnier guedes <albert@teko.net.br>
2026-04-20 17:23:00 -0300

    fix: center author name vertically with JS fallback

--- public/assets/js/pages/authors/index.js
+++ public/assets/js/pages/authors/index.js
@@ -1,4 +1,19 @@
+function verticalCenterSafe(selector) {
+    $(selector).each(function () {
+        if ($(this).css('display') !== 'flex') {
+            $(this).css('display', 'flex');
+        }
+
+        $(this).css({
+            'flex-direction': 'column',
+            'justify-content': 'center'
+        });
+    });
+}
+
 $(function(){
     setUniformHeight('author-name');
     setUniformHeight('author-about');
+    verticalCenterSafe('.author-name');
 });
+
