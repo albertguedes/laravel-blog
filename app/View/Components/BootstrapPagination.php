90edfb481e0ada8b397f3968cf02c60e25baddb1
albert r. carnier guedes <albert@teko.net.br>
2026-04-20 15:52:55 -0300

    fix: rename BootstrapPaginationComponent to BootstrapPagination
    Rename class to match Blade tag naming convention (bootstrap-pagination -> BootstrapPagination)

--- app/View/Components/BootstrapPagination.php
+++ app/View/Components/BootstrapPagination.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class BootstrapPaginationComponent extends Component
+class BootstrapPagination extends Component
 {
     public function __construct(
         public LengthAwarePaginator $paginator,
