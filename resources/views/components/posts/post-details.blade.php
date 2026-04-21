6a247f912e84e7f6cb9e32e027fa2d1c6e531b44
albert r. carnier guedes <albert@teko.net.br>
2026-04-21 19:25:26 -0300

    simplify: remove category and tag links from post details

--- resources/views/components/posts/post-details.blade.php
+++ resources/views/components/posts/post-details.blade.php
@@ -8,29 +8,11 @@
         </h3>
         <small class="text-muted">
             <i class="fa fa-calendar-alt"></i> {{ $post->created_at->format('Y M d') }}
-            @if($post->category)
-                <span class="mx-2">|</span>
-                <i class="fa fa-folder"></i>
-                <a href="{{ route('category', $post->category) }}" class="text-decoration-none">
-                    {{ $post->category->title }}
-                </a>
-            @endif
         </small>
     </header>
 
     <p class="mb-2">
         {{ Str::limit(strip_tags($post->content), 200) }}
     </p>
-
-    @if($post->tags->count() > 0)
-        <footer class="mt-2">
-            @foreach($post->tags as $tag)
-                <a href="{{ route('tag', $tag) }}"
-                   class="badge bg-secondary text-decoration-none me-1">
-                    <i class="fa fa-tag"></i> {{ $tag->title }}
-                </a>
-            @endforeach
-        </footer>
-    @endif
 
 </article>
